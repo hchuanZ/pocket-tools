c73f05b040f691249d3382fe4e32487fd66d9ce8
jerry.zhang <jerry.zhang@spotterio.com>
2024-11-25 19:14:56 +0800

    feat: 溢出jade

--- server/app.js
+++ server/app.js
@@ -13,8 +13,6 @@ var app = express();
 // app.use(express.static(path.join(__dirname, 'dist')));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
